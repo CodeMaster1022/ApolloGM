ca193c3e507af497525f279b139296bc8e4bff12
CodeMaster1022 <CodeMaster.Han1022@gmail.com>
2024-05-06 16:34:18 +0900

    fix

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -1,161 +1,315 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 // material-ui
-import { Grid, Stack, Typography, Box } from '@mui/material';
-import Button from '@mui/material/Button';
 import './landing.css';
-// project import
-import useAuth from 'hooks/useAuth';
-// import AuthWrapper from 'sections/auth/AuthWrapper';
-import AuthLogin from 'sections/auth/auth-forms/AuthLogin';
+import './flow.css';
+import { Grid, Typography, Divider, Box, useMediaQuery } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Logo from 'assets/images/analytics/target.svg';
+import buttonIcon from './button.svg';
 import { makeStyles } from '@material-ui/core/styles';
 import dashboard from '../assets/images/landing/dashboard.jpg';
 import girl from '../assets/images/landing/01.png';
 import AppBar from '@mui/material/AppBar';
-import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
-import AdbIcon from '@mui/icons-material/Adb';
-// import { Email, Key, Lock } from '@mui/icons-material';
-// import InputLabel from 'themes/overrides/InputLabel';
+import twoFlow from 'assets/images/landing/01(3).jpg';
+import threeFlow from 'assets/images/landing/01(8).jpg';
+import fourFlow from 'assets/images/landing/01(10).jpg';
+import fiveFlow from 'assets/images/landing/03(1).jpg';
+import sixFlow from 'assets/images/landing/03(5).jpg';
+import sevenFlow from 'assets/images/landing/04(1).jpg';
 // ================================|| LOGIN ||================================ //
-
+import { Phone, LocationOn, OfflineBolt, Twitter, X, Facebook, RssFeed, Person, PeopleOutline } from '@mui/icons-material';
 const LandingPage = () => {
-  const { isLoggedIn } = useAuth();
+  const isSmScreen = useMediaQuery((theme) => theme.breakpoints.down('md'));
   const classes = useStyles();
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
 
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
+    // <AuthWrapper>
     <>
-      <AppBar sx={{ backgroundColor: '#262626' }}>
-        <Container maxWidth="xl">
-          <Toolbar>
-            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none'
+      <AppBar position="static">
+        <Toolbar variant="dense" sx={{ backgroundColor: '#262626' }}></Toolbar>
+      </AppBar>
+      <Grid container className={classes.image}>
+        <Grid container style={{ display: isSmScreen ? 'none' : 'flex' }}>
+          <Grid item xs={3.1} sx={{ display: 'flex', justifyContent: 'end', alignItems: 'center', height: '50px', paddingX: '28px' }}>
+            <Phone sx={{ fontSize: '18px', color: '#008080' }} />
+            <Typography color="gray"> +800-123-4567 6587</Typography>
+          </Grid>
+          <Grid item xs={5.5} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '50px' }}>
+            <Box
+              style={{
+                padding: '25px',
+                borderLeft: '1px solid #E8E2DC',
+                height: '100% ',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
               }}
             >
-              CapEQ
-            </Typography>
-          </Toolbar>
-        </Container>
-      </AppBar>
+              <LocationOn sx={{ fontSize: '18px', color: '#008080' }} />
+              <Typography color="gray">Beverley, New York 224 USA</Typography>
+            </Box>
+            <Box
+              sx={{
+                paddingX: '25px',
+                borderRight: '1px solid #E8E2DC',
+                height: '100% ',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+              }}
+              gap={4}
+            >
+              <Typography color="gray">Find us on :</Typography>
+              <OfflineBolt
+                sx={{
+                  fontSize: '15px',
+                  color: 'red',
+                  transition: 'all 0.5s',
+                  '&:hover': {
+                    transform: 'scale(1.2) translate(0px, -3px)'
+                  }
+                }}
+              />
+            </Box>
+          </Grid>
+          <Grid item sx={{ width: '50px' }}>
+            <Box
+              sx={{
+                borderRight: '1px solid #E8E2DC',
+                height: '100% ',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <Twitter
+                sx={{
+                  fontSize: '15px',
+                  color: 'red',
+                  transition: 'all 0.5s',
+                  '&:hover': {
+                    transform: 'scale(1.2) translate(0px, -3px)'
+                  }
+                }}
+              />
+            </Box>
+          </Grid>
+          <Grid item sx={{ width: '50px' }}>
+            <Box
+              sx={{
+                borderRight: '1px solid #E8E2DC',
+                height: '100% ',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <X
+                sx={{
+                  fontSize: '13px',
+                  color: 'red',
+                  transition: 'all 0.5s',
+                  '&:hover': {
+                    transform: 'scale(1.2) translate(0px, -3px)'
+                  }
+                }}
+              />
+            </Box>
+          </Grid>
+          <Grid item sx={{ width: '50px' }}>
+            <Box
+              sx={{
+                borderRight: '1px solid #E8E2DC',
+                height: '100% ',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <Facebook
+                sx={{
+                  fontSize: '15px',
+                  color: 'red',
+                  transition: 'all 0.5s',
+                  '&:hover': {
+                    transform: 'scale(1.2) translate(0px, -3px)'
+                  }
+                }}
+              />
+            </Box>
+          </Grid>
+          <Grid item sx={{ width: '50px' }}>
+            <Box
+              sx={{
+                height: '100% ',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <RssFeed
+                sx={{
+                  fontWeight: '1500px',
+                  fontSize: '18px',
+                  color: 'red',
+                  transition: 'all 0.5s',
+                  '&:hover': {
+                    transform: 'scale(1.2) translate(0px, -3px)'
+                  }
+                }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+        <Divider color="#E1E2DC" />
+        <AppBar position="sticky" sx={{ backgroundColor: 'transparent', boxShadow: '0px 0px 0px 0px' }}>
+          <Box
+            sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', width: '100%', height: '92px' }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '19px',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backdropFilter: isScrolled ? 'blur(20px)' : 'none',
+                boxShadow: isScrolled ? '2px 3px 2px 3px' : '0px 0px 0px 0px'
+              }}
+            >
+              <Box sx={{ width: '1330px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex' }}>
+                  <img src={Logo} alt="logo" />
+                  <span>
+                    <Typography sx={{ fontSize: '35px', color: '#1890FF', fontWeight: '700' }}>CapCE</Typography>
+                  </span>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }} gap={4}>
+                  <Typography sx={{ fontSize: '20px', color: 'gray', fontWeight: '700' }}>Home</Typography>
+                  <Typography sx={{ fontSize: '20px', color: 'gray', fontWeight: '700' }}>Blog</Typography>
+                  <Typography sx={{ fontSize: '20px', color: 'gray', fontWeight: '700' }}>Contact</Typography>
+                  <Typography sx={{ fontSize: '20px', color: 'gray', fontWeight: '700' }}>Pages</Typography>
+                  <Typography sx={{ fontSize: '20px', color: 'gray', fontWeight: '700' }}>Courses</Typography>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <Button sx={{ backgroundColor: 'white', height: '50px', color: '#F16126', borderRadius: '0px', padding: '20px' }}>
+                    <Person />
+                    LOGIN
+                  </Button>
+                  <Button sx={{ backgroundColor: '#F16126', height: '50px', color: 'white', borderRadius: '0px' }}>
+                    <PeopleOutline />
+                    SIGN UP
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </AppBar>
+        <Box sx={{ height: '50px' }} />
+        <Grid sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Grid container sx={{ maxWidth: '1330px' }} justifyContent="center" alignItems="center">
+            <Grid item sx={{ width: '500px' }}>
+              <Typography variant="h1" sx={{ fontSize: 66, fontWeight: 680, padding: 0 }}>
+                Learn The Skills You Need To Succeed
+              </Typography>
+              <Typography variant="h4" fontWeight="light">
+                Free online courses from the world’s Leading experts. join 18+ million Learners today.
+              </Typography>
+              <Typography fontSize={20}></Typography>
+              <Box sx={{ paddingY: '15px', width: '100%' }}>
+                <button className="button">
+                  <img src={buttonIcon} alt="icon" style={{ width: 15, height: 15 }} />
+                  Get Started
+                </button>
+              </Box>
+            </Grid>
+            {/* </Box> */}
+            <Grid item sx={{ flex: 1 }}>
+              <div className="star" />
+              <div className="star" />
+              <div className="star" />
+              <div className="star" />
+              <div className="star" />
+              <Box sx={{ position: 'absolute', zIndex: 1, marginLeft: '8vh', marginTop: '5vh' }}>
+                <div className="dashcircle"></div>
+                <div className="dotthird"></div>
+              </Box>
+              <Box sx={{ position: 'absolute', zIndex: 1, marginLeft: '12vh', marginTop: '9vh' }}>
+                <div className="dashcircleTwo"></div>
+                <div className="dotsecond"></div>
+              </Box>
+              <Box sx={{ position: 'absolute', zIndex: 1, marginLeft: '16vh', marginTop: '13vh' }}>
+                <div className="dashcircleThree"></div>
+                <div className="dotfirst" />
+              </Box>
+              <Box sx={{ position: 'relative', zIndex: 2, '& img': { marginBottom: -1 } }}>
+                <img src={girl} alt="girl" />
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Box sx={{ height: '120px', backgroundColor: '#FFF9F1' }}></Box>
+      </Grid>
+      <Grid display="flex" justifyContent="center">
+        <Grid container justifyContent="center" alignItems="center" maxWidth="1330px">
+          <div className="content">
+            <div className="benefits">
+              <div className="basic-marquee basic-marquee-1">
+                <button className="button">
+                  <img src={twoFlow} alt="one" />
+                </button>
+                <button className="button">
+                  <img src={threeFlow} alt="one" />
+                </button>
+                <button className="button">
+                  <img src={fourFlow} alt="one" />
+                </button>
+                <button className="button">
+                  <img src={fiveFlow} alt="one" />
+                </button>
+                <button className="button">
+                  <img src={sixFlow} alt="one" />
+                </button>
+                <button className="button">
+                  <img src={sevenFlow} alt="one" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
     </>
-    // <AuthWrapper>
-    // <>
-
-    //   <Grid container className={classes.image} sx={{ marginTop: '60px' }}>
-    //     <Box sx={{ height: '100px' }}>
-    //       <h1>Hello</h1>
-    //     </Box>
-    //     <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-    //       <Grid container>
-    //         <Grid item xs={12} md={5} paddingX={5}>
-    //           <Box>
-    //             <Typography fontSize={50} fontWeight={680}>
-    //               Design what you love
-    //             </Typography>
-    //           </Box>
-    //           <Typography fontSize={20}>
-    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non libero dignissim, viverra augue eu.
-    //           </Typography>
-    //           <Box sx={{ display: 'flex', padding: '15px' }}>
-    //             <button className="btn" type="button">
-    //               <strong>Get Started</strong>
-    //               <div id="container-stars">
-    //                 <div id="stars"></div>
-    //               </div>
-    //               <div id="glow">
-    //                 <div className="circle"></div>
-    //                 <div className="circle"></div>
-    //               </div>
-    //             </button>
-    //           </Box>
-    //           {/* <Button sx={{ background: '#FE6E06', color: 'white', borderRadius: '20px', paddingY: '13px', paddingX: '25px' }}>
-    //             Get Started
-    //           </Button> */}
-    //         </Grid>
-    //         <Grid item xs={12} md={5} paddingX={2}>
-    //           <div className="star" />
-    //           <div className="star" />
-    //           <div className="star" />
-    //           <div className="star" />
-    //           <div className="star" />
-    //           <Box sx={{ position: 'absolute', zIndex: 1, marginLeft: '5vh', marginTop: '5vh', }}>
-    //             <div className="dashcircle"></div>
-    //             <div className="dotthird"></div>
-    //           </Box>
-    //           <Box sx={{ position: 'absolute', zIndex: 1, marginLeft: '9vh', marginTop: '9vh' }}>
-    //             <div className="dashcircleTwo"></div>
-    //             <div className="dotsecond"></div>
-    //           </Box>
-    //           <Box sx={{ position: 'absolute', zIndex: 1, marginLeft: '13vh', marginTop: '13vh' }}>
-    //             <div className="dashcircleThree"></div>
-    //             <div className="dotfirst" />
-    //           </Box>
-    //           {/* <Box sx={{ position: 'absolute', zIndex: 1, marginLeft: '13vh', marginTop: '13vh' }}>
-    //             <div className="box"></div>
-    //           </Box> */}
-    //           <Box alt="hello" className={classes.imagegirl} sx={{ width: { xs: '100%' }, height: { xs: '350px', sm: '70vh' } }} />
-    //         </Grid>
-    //         <Grid item xs={12} md={2} paddingX={2} gap={5}>
-    //           <button className="button">
-    //             <span className="text">Message</span>
-    //             <span className="icon">
-    //               <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" data-icon="paper-plane" width="20px" aria-hidden="true">
-    //                 <path
-    //                   d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"
-    //                   fill="currentColor"
-    //                 ></path>
-    //               </svg>
-    //             </span>
-    //           </button>
-    //           <button className="button">
-    //             <span className="text">Message</span>
-    //             <span className="icon">
-    //               <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" data-icon="paper-plane" width="20px" aria-hidden="true">
-    //                 <path
-    //                   d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"
-    //                   fill="currentColor"
-    //                 ></path>
-    //               </svg>
-    //             </span>
-    //           </button>
-    //           <button className="button">
-    //             <span className="text">Message</span>
-    //             <span className="icon">
-    //               <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" data-icon="paper-plane" width="20px" aria-hidden="true">
-    //                 <path
-    //                   d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"
-    //                   fill="currentColor"
-    //                 ></path>
-    //               </svg>
-    //             </span>
-    //           </button>
-    //         </Grid>
-    //       </Grid>
-    //     </Box>
-    //   </Grid>
-    // </>
-    // // </AuthWrapper>
+    // </AuthWrapper>
   );
 };
 {
   /* <Box sx={{ width: { xs: 650, md: 400 } }}> */
 }
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh'
-  },
+  // root: {
+  //   height: '100vh'
+  // },
   paper: {
     margin: theme.spacing(1, 4),
     maxWidth: '500px',
@@ -192,6 +346,8 @@ const useStyles = makeStyles((theme) => ({
     color: '#7E8487'
   },
   image: {
+    display: 'flex',
+    flexDirection: 'column',
     backgroundImage: `url(${dashboard})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
@@ -203,7 +359,7 @@ const useStyles = makeStyles((theme) => ({
     zIndex: 2,
     backgroundImage: `url(${girl})`,
     backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
+    backgroundSize: 'contain cover',
     backgroundPosition: 'bottom'
   }
 }));
